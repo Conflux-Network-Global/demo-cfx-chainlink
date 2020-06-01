5978f30ef46e145185dccd54d73b26c35970a33f
aalu1418 <aalu1418@gmail.com>
2020-06-01 16:52:41 -0400

    complete cycle achieved on testnet but sdk doesn't capture completed transactions - will test on mainnet endpoint tomorrow

---
+++ CFX_externalAdapter/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Conflux } = require("js-conflux-sdk");
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const main = () => {
+  app.use(bodyParser.json());
+
+  app.post("/", async (req, res) => {
+    console.log("POST Data: ", req.body);
+    const output = { id: req.body.id };
+    const token = req.headers.authorization.split(" ")[1]
+    if (token !== process.env.TOKEN) {
+      res.status(401).send("invalid token");
+    }
+
+    //create conflux instance
+    const cfx = new Conflux({
+      // url: "http://mainnet-jsonrpc.conflux-chain.org:12537",
+      url: "http://testnet-jsonrpc.conflux-chain.org:12537",
+      defaultGasPrice: 100,
+      defaultGas: 1000000,
+      logger: console,
+    });
+
+    const account = cfx.Account(PRIVATE_KEY); // create account instance
+    console.log(account.address); // 0x1bd9e9be525ab967e633bcdaeac8bd5723ed4d6b
+
+    // create contract instance
+    const contract = cfx.Contract({
+      abi: require("../contractInteraction/contract/abi.json"), //can be copied from remix
+      // address: "0x8aa73841e0a0e6e816b2c66c9c5ed1e144ad8cbb",
+      address: "0x8d6fd7de324a2ac33c753d7c80f79d9afdc42db2",
+    });
+
+    // interact with contract
+    const receipt = await contract
+      .update(req.body.data.result)
+      .sendTransaction({ from: account })
+      .confirmed( );
+    console.log(receipt);
+
+    res.status(200).send(output);
+  });
+
+  app.listen(port, () => console.log(`${port} is active`));
+
+  process.on("SIGINT", () => {
+    process.exit();
+  });
+};
+
+main();
--- commands.md
+++ commands.md
@@ -1,5 +1,5 @@
 ```
-./external-initiator "{\"name\":\"cfx-mainnet\",\"type\":\"conflux\",\"url\":\"http://mainnet-jsonrpc.conflux-chain.org:12537\"}" --chainlinkurl "http://localhost:6688/"
+./external-initiator "{\"name\":\"cfx-mainnet\",\"type\":\"conflux\",\"url\":\"http://testnet-jsonrpc.conflux-chain.org:12537\"}" --chainlinkurl "http://localhost:6688/"
 ```
 
 ```
